8115c1d54dddb501440c7da54628c2a72d2ab0af
Rafael-GSousa <rafael.sousa37@fatec.sp.gov.br>
2022-11-01 19:56:37 -0300

    Update

---
+++ Python/pythonDesafios/020.py
@@ -0,0 +1,3 @@
+# O mesmo professor do defaio anterior quer sortear a ordem de
+# apresentação de trabalhos dos alunos. Faça um programa que leia
+# o nome dos quatro alunos e mostre a ordem sorteada.
---
+++ Python/pythonDesafios/021.py
@@ -0,0 +1,2 @@
+# Faça um programa em Python que abra e reproduza o áudio de
+# um arquivo mp3
