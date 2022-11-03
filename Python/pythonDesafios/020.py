9d8078922f0bdfac48d486a68ad7646557e8050e
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-02 23:45:10 -0300

    Update

--- Python/pythonDesafios/020.py
+++ Python/pythonDesafios/020.py
@@ -2,4 +2,12 @@
 # apresentação de trabalhos dos alunos. Faça um programa que leia
 # o nome dos quatro alunos e mostre a ordem sorteada.
 
-from random import random
+from random import shuffle
+
+a1 = input('1º Aluno: ')
+a2 = input('2º Aluno: ')
+a3 = input('3º Aluno: ')
+a4 = input('4º Aluno: ')
+lista = [a1, a2, a3, a4]
+shuffle(lista)
+print(f'Ordem {lista}')
--- Python/pythonDesafios/021.py
+++ Python/pythonDesafios/021.py
@@ -1,2 +1,16 @@
 # Faça um programa em Python que abra e reproduza o áudio de
 # um arquivo mp3
+
+import pygame
+# iniciando o pygame
+pygame.init()
+# chamando a música
+pygame.mixer.music.load("021.mp3")
+# dando play na música
+pygame.mixer.music.play()
+# comando input() para reproduzir a música
+# tendo em vista que houve atualização no pygame
+# e só o comando acima não reproduz a música de fato
+input()
+# finaliza a reprodução
+pygame.event.wait()
