c227090e0ebbcef53c95b58152a3a26b286d0d4e
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-03 17:13:08 -0300

    Update

--- Python/pythonDesafios/022.py
+++ Python/pythonDesafios/022.py
@@ -3,10 +3,19 @@
 # ==> O nome com todas as letras minúsculas
 # ==> Quantas letras no total (sem considerar os espaços)
 # ==> Quantas letras tem o primeiro nome
+
+# Entrada de dados pelo teclado
 nome = input('Digite um nome completo: ')
+
+# Variável alternativa para juntar um nome com ou outro que serão quebrados no método split()
 juntaNome = ""
+
+# estrutura de repetição para quebrar o nome no split e depois concatenar suas posições
 for i in range(0, len(nome.split())):
     juntaNome = juntaNome + (nome.split()[i])
+
+# mostrando na tela os resultados
+# utilizando um único print com 3 aspas duplas sem necessidade de usar quebra de linha
 print(f"""Nome digitado ==> {nome}
 Todas as letras maiúsculas ==> {nome.upper()}
 Todas as letras minúsculas ==> {nome.lower()}
