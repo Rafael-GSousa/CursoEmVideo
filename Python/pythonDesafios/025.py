55288305b429b6056ed4880ce948338031eb4b33
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-03 18:21:39 -0300

    Update

--- Python/pythonDesafios/025.py
+++ Python/pythonDesafios/025.py
@@ -1,2 +1,4 @@
 # Crie um programa que leia o nome de uma pessoa
 # e diga se ela tem "SILVA" no nome
+nome = input('Digite um nome completo: ')
+print(f'Tem "SILVA" no nome? ==> {"SILVA" in nome}')
