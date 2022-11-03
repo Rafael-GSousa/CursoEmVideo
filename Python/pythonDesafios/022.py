5496656d0cc80acac3447c913493d8c7817af4fc
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-03 17:00:14 -0300

    Update

--- Python/pythonDesafios/022.py
+++ Python/pythonDesafios/022.py
@@ -3,3 +3,12 @@
 # ==> O nome com todas as letras minúsculas
 # ==> Quantas letras no total (sem considerar os espaços)
 # ==> Quantas letras tem o primeiro nome
+nome = input('Digite um nome completo: ')
+juntaNome = ""
+for i in range(0, len(nome.split())):
+    juntaNome = juntaNome + (nome.split()[i])
+print(f"""Nome digitado ==> {nome}
+Todas as letras maiúsculas ==> {nome.upper()}
+Todas as letras minúsculas ==> {nome.lower()}
+Quantas letras no total (sem os espaços) ==> {len(juntaNome)}
+Quantas letras têm no primeiro nome ==> {len(nome.split()[0])}""")
