bbbd74a4f58ef358b332be40e4def76777614516
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-03 18:41:35 -0300

    Update

--- Python/pythonDesafios/026.py
+++ Python/pythonDesafios/026.py
@@ -2,3 +2,7 @@
 # ==> Quantas vezes aparece a letra "A"
 # ==> Em que posição ela aparece a primeira vez
 # ==> Em que posição ela aparece a última vez
+frase = input('Digite uma frase: ')
+print(f"""Quantas vezes aparece a letra "A" ==> {frase.count('A')} 
+Em que posição ela aparece a primeira vez ==> {frase.find('A')} 
+Em que posição ela aparece a última vez ==> {frase.rfind('A')}""")
