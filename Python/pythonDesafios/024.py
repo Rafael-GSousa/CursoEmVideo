25c04fa872d2420e2e60aee682a6bc47a559fedd
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-04 19:34:57 -0300

    Update 024

--- Python/pythonDesafios/024.py
+++ Python/pythonDesafios/024.py
@@ -1,5 +1,6 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa
 # ou não com o nome "SANTO"
 
-cidade = input('Digite o nome da cidade: ')
-print(f'Começa com "SANTO"? ==> {cidade.startswith("SANTO")}')
+cidade = str(input('Digite o nome da cidade: ')).strip()
+print(f'Começa com "SANTO"? ==> {cidade.upper().startswith("SANTO")}')
+print(f'Começa com "SANTO"? ==> {cidade[0:5].upper() == "SANTO"}')
