982d34c42723889f95d1daabbc12df20dec69abf
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-07 20:07:07 -0300

    add cores no terminal python

---
+++ Python/PythonAulas/CoresNoTerminal/aula011.py
@@ -0,0 +1,19 @@
+# ANSI cores no terminal do python
+
+# print('\033[0;39;41m Teste')
+# print('\033[4;33;44m Teste')
+# print('\033[1;35;43m Teste')
+# print('\033[39;42m Teste')
+# print('\033[m Teste')
+# print('\033[7;39m Teste')
+
+print('\033[31m Olá, Mundo!\033[m')
+print('\033[1;43m Olá, Mundo!\033[m')
+print('\033[1;31;43m Olá, Mundo!\033[m')
+print('\033[0;39;40m Olá, Mundo!\033[m')
+print('\033[4;39;45m Olá, Mundo!\033[m')
+print('\033[7;39m Olá, Mundo!\033[m')
+
+a = 3
+b = 5
+print(f'Os valores são \033[32;40m{a}\033[m e \033[31;40m{b}\033[m!!!\033[m')
