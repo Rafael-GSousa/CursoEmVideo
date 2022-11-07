fc6b18c54e8500e5f8ec87890a74e114ab0dfe27
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-07 18:25:25 -0300

    atualizando desafio 33

--- Python/pythonDesafios/033.py
+++ Python/pythonDesafios/033.py
@@ -4,27 +4,19 @@
 n1 = float(input('Digite o 1º número: '))
 n2 = float(input('Digite o 2º número: '))
 n3 = float(input('Digite o 3º número: '))
+# Definindo o maior e o menor inicial
+maior = n1
+menor = n1
 
-maior = 0
-menor = 0
-
-if n1 > n2 > n3:
-    maior = n1
-    menor = n3
-if n1 > n3 > n2:
-    maior = n1
+# Verificando o menor
+if n2 < n1 and n2 < n3:
     menor = n2
-if n2 > n3 > n1:
-    maior = n2
-    menor = n1
-if n2 > n1 > n3:
-    maior = n2
+if n3 < n1 and n3 < n2:
     menor = n3
-if n3 > n1 > n2:
+# Verificando o maior
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
     maior = n3
-    menor = n2
-if n3 > n2 > n1:
-    maior = n3
-    menor = n1
 
 print(f'Maior = {maior}, menor = {menor}')
