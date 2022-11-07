c5756b83160086543956cf0298d93cb5ec990ec0
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-07 00:08:43 -0300

    atualizando desafio 30

---
+++ Python/pythonDesafios/030.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia um número inteiro e mostre na tela
+# se ele é PAR ou IMPAR
+
+n = int(input('Digite um número inteiro: '))
+if n % 2 == 0:
+    print(f'{n} é PAR!')
+else:
+    print(f'{n} é IMPAR!')
