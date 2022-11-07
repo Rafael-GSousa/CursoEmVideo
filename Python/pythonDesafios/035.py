ae667b2a45e5d4a322bcdeb504c564fb1d6639c1
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-07 19:22:48 -0300

    atualizando desafio 35

--- Python/pythonDesafios/035.py
+++ Python/pythonDesafios/035.py
@@ -5,7 +5,7 @@ r1 = float(input('Comprimento da 1ª reta: '))
 r2 = float(input('Comprimento da 2ª reta: '))
 r3 = float(input('Comprimento da 3ª reta: '))
 
-if (r1 + r2) > r3 or (r1 + r3) > r2 or (r2 + r3) > r1 or (r1 - r2) < r3 or (r1 - r3) < r2 or (r2 - r3) < r1:
+if (r1 + r2) > r3 and (r1 + r3) > r2 and (r2 + r3) > r1:
     print('As retas podem formar um triângulo!')
 else:
     print('As retas não podem formar um triângulo!')
