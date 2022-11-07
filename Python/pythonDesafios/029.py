6df612bb11d3010a97bba92654a30cf1387be446
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-07 00:05:06 -0300

    atualizando desafio 29

---
+++ Python/pythonDesafios/029.py
@@ -0,0 +1,10 @@
+# Escreva um programa que leia a velocidade de um carro.
+# Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele
+# foi multado.
+# A multa vai custar R$7,00 por cada Km acima do limite.
+
+v = float(input('Qual a velocidade percorrida pelo carro?\n'))
+if v > 80:
+    m = (v - 80) * 7
+    print(f"""Você ultrapassou em {v - 80:.2f}Km do limite e 
+    foi multado em R${m:.2f}""")
