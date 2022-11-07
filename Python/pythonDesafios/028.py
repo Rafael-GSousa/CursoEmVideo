252e88f86e103f4ea3925656e3ad4f73efdfa601
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-07 17:03:07 -0300

    atualizando desafio 28

--- Python/pythonDesafios/028.py
+++ Python/pythonDesafios/028.py
@@ -3,10 +3,18 @@
 # escolhido pelo computador.
 # O programa deverá escrever na tela se o usuário venceu ou perdeu
 from random import randint
+from time import sleep
 
-n = randint(0, 5)
-num = int(input('Digite um número entre 0 e 5: '))
+n = randint(0, 5)  # número inteiro aleatório gerado pelo computador
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente advinhar...')
+print('-=-' * 20)
+num = int(input('Em qual número eu pensei?  '))
+print('Processando...')
+sleep(2)
+print(f"""COMPUTADOR ==> {n}
+JOGADOR ==> {num}""")
 if num == n:
-    print('Parabéns! Você venceu!')
+    print('VOCÊ GANHOU! Parabéns!')
 else:
-    print('Que pena! Você perdeu!')
+    print('GANHEI! Que pena, você perdeu!')
