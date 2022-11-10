d5aa928a8aadc16704ff55427e39f05e97950945
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-10 20:10:12 -0300

    update cores no terminal python

--- Python/PythonAulas/CoresNoTerminal/aula011.py
+++ Python/PythonAulas/CoresNoTerminal/aula011.py
@@ -1,19 +1,40 @@
 # ANSI cores no terminal do python
-
-# print('\033[0;39;41m Teste')
-# print('\033[4;33;44m Teste')
-# print('\033[1;35;43m Teste')
-# print('\033[39;42m Teste')
-# print('\033[m Teste')
-# print('\033[7;39m Teste')
-
-print('\033[31m Olá, Mundo!\033[m')
-print('\033[1;43m Olá, Mundo!\033[m')
-print('\033[1;31;43m Olá, Mundo!\033[m')
-print('\033[0;39;40m Olá, Mundo!\033[m')
-print('\033[4;39;45m Olá, Mundo!\033[m')
-print('\033[7;39m Olá, Mundo!\033[m')
-
-a = 3
-b = 5
-print(f'Os valores são \033[32;40m{a}\033[m e \033[31;40m{b}\033[m!!!\033[m')
+# style = {'reset': '\033[0m',
+#          'bold': '\033[01m',
+#          'disable': '\033[02m',
+#          'underline': '\033[04m',
+#          'reverse': '\033[07m',
+#          'invisible': '\033[08m',
+#          'strikethrough': '\033[09m'}
+# fontcolor = {'black': '\033[30m',
+#              'red': '\033[31m',
+#              'green': '\033[32m',
+#              'orange': '\033[33m',
+#              'blue': '\033[34m',
+#              'purple': '\033[35m',
+#              'cyan': '\033[36m',
+#              'lightgrey': '\033[37m',
+#              'darkgrey': '\033[90m',
+#              'lightred': '\033[91m',
+#              'lightgreen': '\033[92m',
+#              'yellow': '\033[93m',
+#              'lightblue': '\033[94m',
+#              'pink': '\033[95m',
+#              'lightcyan': '\033[96m'}
+# background = {'black': '\033[40m',
+#               'red': '\033[41m',
+#               'green': '\033[42m',
+#               'orange': '\033[43m',
+#               'blue': '\033[44m',
+#               'purple': '\033[45m',
+#               'cyan': '\033[46m',
+#               'lightgrey': '\033[47m'}
+texto = str(input('Digite qualquer coisa: '))
+estilo = int(input('Estilo do texto: '))
+estilo = f'\033[{estilo}m'
+cor = int(input('Cor do texto: '))
+cor = f'\033[{cor}m'
+fundo = int(input('Cor de fundo: '))
+fundo = f'\033[{fundo}m'
+print(f"""Normal ==> {texto}
+Formatado ==> {estilo + cor + fundo + texto}\033[m""")
