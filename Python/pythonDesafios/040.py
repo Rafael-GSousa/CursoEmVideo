5955212f803c1ca2d261001a745844a6a17bcea2
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-12 18:15:23 -0300

    Add desafio 40

---
+++ Python/pythonDesafios/040.py
@@ -0,0 +1,29 @@
+# Crie um programa que leia duas notas de um aluno
+# e calcule a sua média, mostrando uma mensagem no final,
+# de acordo com a média atingida:
+# - Média abaixo de 5.0: REPROVADO
+# - Média entre 5.0 e 6.9: RECUPERAÇÃO
+# - Média 7.0 ou superior: APROVADO
+aluno = str(input('Nome do(a) aluno(a) ==> '))
+n1 = float(input(f'1ª nota do(a) aluno(a) {aluno} ==> '))
+n2 = float(input(f'2ª nota do(a) aluno(a) {aluno}  ==> '))
+
+media = (n1 + n2) / 2
+msg = ''
+
+if media < 5.0:
+    msg = f"""Aluno(a) ==> {aluno}
+1ª e 2ª nota ==> {n1} e {n2}
+Média ==> {media}
+Situação ==> REPROVADO(A)"""
+elif 5.0 <= media <= 6.9:
+    msg = f"""Aluno(a) ==> {aluno}
+1ª e 2ª nota ==> {n1} e {n2}
+Média ==> {media}
+Situação ==> RECUPERAÇÃO"""
+else:
+    msg = f"""Aluno(a) ==> {aluno}
+1ª e 2ª nota ==> {n1} e {n2}
+Média ==> {media}
+Situação ==> APROVADO(A)"""
+print(msg)
