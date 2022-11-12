039b2a3fb7d5d58e258f54f37bb7902c364dbf62
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-12 19:28:32 -0300

    update desafio 45

--- Python/pythonDesafios/045.py
+++ Python/pythonDesafios/045.py
@@ -7,9 +7,15 @@ usuario = str(input('Pedra, Papel ou Tesoura? ')).strip().upper()
 print(f"""Escolha do computador ==> {computador}
 Sua escolha ==> {usuario}""")
 
-if computador == 'PEDRA' and usuario == 'TESOURA' or computador == 'PAPEL' and usuario == 'PEDRA' or computador == 'TESOURA' and usuario == 'PAPEL':
+if (computador == 'PEDRA' and usuario == 'TESOURA' or
+        computador == 'PAPEL' and usuario == 'PEDRA' or
+        computador == 'TESOURA' and usuario == 'PAPEL'):
     print(f'O computador venceu!')
-elif usuario == 'PEDRA' and computador == 'TESOURA' or usuario == 'PAPEL' and computador == 'PEDRA' or usuario == 'TESOURA' and computador == 'PAPEL':
+elif (usuario == 'PEDRA' and computador == 'TESOURA' or
+      usuario == 'PAPEL' and computador == 'PEDRA' or
+      usuario == 'TESOURA' and computador == 'PAPEL'):
     print('Você venceu!')
-else:
+elif computador == usuario:
     print('Empate. Jogue novamente!')
+else:
+    print(f'{usuario} não existe no Jokenpô')
