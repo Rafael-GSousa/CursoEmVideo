a158df0724feaccb51208194bdec02326251766c
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-14 17:27:57 -0300

    update desafio 42

--- Python/pythonDesafios/042.py
+++ Python/pythonDesafios/042.py
@@ -3,3 +3,20 @@
 # - Equilátero: todos os lados iguais
 # - Isósceles: dois lados iguais
 # - Escaleno: todos os lados diferentes
+r1 = float(input('Comprimento da 1ª reta ==> '))
+r2 = float(input('Comprimento da 2ª reta ==> '))
+r3 = float(input('Comprimento da 3ª reta ==> '))
+
+tipo = ''
+msg = f'Será formado um triângulo do tipo {tipo}'
+
+if (r1 + r2) > r3 and (r1 + r3) > r2 and (r2 + r3) > r1:
+    if r2 == r1 == r3:
+        tipo = 'Equilátero, pois, todos os lados são iguais'
+    elif r2 == r1 != r3 or r2 != r1 == r3 or r1 != r2 == r3:
+        tipo = 'Isósceles, pois, apenas dois lados são iguais'
+    else:
+        tipo = 'Escaleno, pois, todos os lados são diferentes'
+else:
+    msg = 'Não se pode formar um triângulo com esses segmentos de reta'
+print(msg + tipo)
