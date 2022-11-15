66124f48add10dbb3721d871bad8bedde473f898
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-15 01:21:26 -0300

    update desafio 44

--- Python/pythonDesafios/044.py
+++ Python/pythonDesafios/044.py
@@ -6,33 +6,43 @@
 # - 3x ou mais no cartão: 20% de juros
 from time import sleep
 produto = float(input('Qual o valor do produto? R$ '))
-sleep(2)
-pagamento = int(input(f"""Formas de pagamento:
+opcao = int(input(f"""Formas de pagamento:
 1 - À vista dinheiro / cheque: 10% de desconto
 2 - À vista no cartão: 5% de desconto
 3 - Em até 2x no cartão: preço normal
 4 - 3x ou mais no cartão: 20% de juros
 Selecione a opção desejada ==> """))
 
-valor = 0
-forma = ''
 
-if pagamento == 1:
-    valor = produto * 0.9
-    forma = 'À vista dinheiro / cheque: 10% de desconto'
-elif pagamento == 2:
-    valor = produto * 0.95
-    forma = 'À vista no cartão: 5% de desconto'
-elif pagamento == 3:
-    valor = produto
-    forma = 'Em até 2x no cartão: preço normal'
-elif pagamento == 4:
-    valor = produto * 1.20
-    forma = '3x ou mais no cartão: 20% de juros'
+if opcao == 1:
+    sleep(2)
+    print(f'''Valor da compra ==> R${produto:.2f}
+Forma de pagamento ==> À vista dinheiro / cheque
+Desconto ==> R${produto * 0.1:.2f} (10%)
+Total a pagar ==> R${produto * 0.9:.2f}''')
+elif opcao == 2:
+    sleep(2)
+    print(f'''Valor da compra ==> R${produto:.2f}
+Forma de pagamento ==> À vista no cartão
+Desconto ==> R${produto * 0.05:.2f} (5%)
+Total a pagar ==> R${produto * 0.95:.2f}''')
+elif opcao == 3:
+    sleep(2)
+    print(f'''Valor da compra ==> R${produto:.2f}
+Forma de pagamento ==> Em até 2x no cartão s/ juros 
+Total a pagar ==> R${produto:.2f}''')
+elif opcao == 4:
+    parcelas = int(input('Qual é a quantidade de parcelas? '))
+    if 3 <= parcelas <= 10:
+        sleep(2)
+        print(f'''Valor da compra ==> R${produto:.2f}
+Forma de pagamento ==> 3x ou mais no cartão
+Juros ==> R${produto * 0.2:.2f} (20%)
+Nº de parcelas ==> {parcelas}
+Valor da parcela ==> R${produto * 1.2 / parcelas:.2f}
+Total a pagar ==> R${produto * 1.2:.2f}''')
+    else:
+        print('Número de parcelas inválido!')
+        exit()
 else:
     print('Forma de pagamento inválida! Tente novamente!')
-    exit()
-sleep(3)
-print(f"""Valor do produto ==> R${produto:.2f}
-Forma de pagamento ==> {pagamento} - {forma}
-Total a pagar ==> R${valor:.2f}""")
