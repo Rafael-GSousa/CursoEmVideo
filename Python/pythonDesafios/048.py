5ed7f3f421d97e0b1f2fe51d16aceb69e07ae445
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-16 13:39:06 -0300

    update desafio 48

--- Python/pythonDesafios/048.py
+++ Python/pythonDesafios/048.py
@@ -2,8 +2,8 @@
 # que são múltiplos de três e que se encontram no intervalo de 1 até 500
 soma = 0
 cont = 0
-for i in range(1, 501):
-    if i % 3 == 0 and i % 2 == 1:
+for i in range(1, 501, 2):
+    if i % 3 == 0:
         soma += i
         cont += 1
 print(f'A soma dos {cont} números ímpares, múltiplos de 3, entre 1 e 500 é {soma}')
