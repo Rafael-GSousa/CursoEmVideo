40ae944a7d9a310a9a723aa314f34970eccae3d5
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-16 01:23:50 -0300

    update desafio 46

--- Python/pythonDesafios/046.py
+++ Python/pythonDesafios/046.py
@@ -1,6 +1,7 @@
 # Faça um programa que mostre na tela uma contagem regressiva
 # para o estouro de fogos de artifício, indo de 10 até 0,
 # com uma pausa de 1 segundo entre eles.
+from emoji import emojize
 from time import sleep
 print('Contagem regressiva...')
 sleep(1)
@@ -8,4 +9,4 @@ for i in range(10, 0, -1):
     sleep(1)
     print(i)
 sleep(1)
-print('Boom ****')
+print('Boom', emojize('\U0001f386'))
