1318f4d8bb02264a3f5f7bedc25509ffdcf16c7c
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-16 15:13:21 -0300

    update desafio 53

--- Python/pythonDesafios/053.py
+++ Python/pythonDesafios/053.py
@@ -3,10 +3,15 @@
 frase = str(input('Digite uma frase: ')).strip().upper()
 palavras = frase.split()
 junto = ''.join(palavras)
-inverso = ''
-for letra in range(len(junto) - 1, -1, -1):
-    inverso += junto[letra]
-if inverso == frase:
+# inverso = ''
+
+# com for
+'''for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]'''
+
+# sem for
+inverso = junto[::-1]
+if inverso == junto:
     print(f'{frase} é um palíndromo')
 else:
     print(f'{frase} não é um palíndromo')
