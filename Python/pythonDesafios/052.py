1bbd5ff2aaae93f8956afa28052443f5b959646b
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-16 14:50:54 -0300

    update desafio 52

--- Python/pythonDesafios/052.py
+++ Python/pythonDesafios/052.py
@@ -1,14 +1,16 @@
 # Faça um programa que leia um número inteiro e diga se ele
 # é ou não um número primo.
 n = int(input('Digite um número inteiro: '))
-
-if n > 1:
-    for i in range(2, int(n / 2) + 1):
-        if (n % i) == 0:
-            print(f'{n} não é primo!')
-            break
+total = 0
+for i in range(1, n + 1):
+    if n % i == 0:
+        print('\033[34m', end='')
+        total += 1
     else:
-        print(f'{n} é primo!')
-
+        print('\033[m', end='')
+    print(i, end=' ')
+if total == 2:
+    msg = 'É UM NÚMERO PRIMO'
 else:
-    print(f'{n} não é primo!')
+    msg = 'NÃO É UM NÚMERO PRIMO'
+print(f'\n\033[mO número {n} foi divisível {total} vezes. Portanto ele \033[34m{msg}!')
