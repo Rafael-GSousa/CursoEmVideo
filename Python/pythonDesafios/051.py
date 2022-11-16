d5056acf75c7307b775fab6707d6855db9e76272
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-16 14:30:44 -0300

    update desafio 51

--- Python/pythonDesafios/051.py
+++ Python/pythonDesafios/051.py
@@ -1,8 +1,10 @@
 # Desenvolva um programa que leia o primeiro termo e a razão
 # de uma PA(Progressão Aritmética). No final, mostre os 10 primeiros
 # termos dessa progressão.
-
-pTermo = int(input('1º termo de uma PA: '))
+print('=' * 30)
+print('{:^30}'.format('10 termos de uma PA'))
+print('=' * 30)
+pTermo = int(input('1º termo: '))
 razao = int(input('Razão da PA: '))
 termos = 10
 uTermo = pTermo + (termos-1) * razao
