fa6905d73b614ea4ce0d1f59c5b3df7f51f05574
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-16 01:08:28 -0300

    Add aula 013 e desafio 46

---
+++ Python/PythonAulas/EstruturaDeRepeticaoFor/aula013.py
@@ -0,0 +1,4 @@
+for i in range(6, 0, -1):
+    print(i)
+print('Fim!')
+
---
+++ Python/pythonDesafios/046.py
@@ -0,0 +1,11 @@
+# Faça um programa que mostre na tela uma contagem regressiva
+# para o estouro de fogos de artifício, indo de 10 até 0,
+# com uma pausa de 1 segundo entre eles.
+from time import sleep
+print('Contagem regressiva...')
+sleep(1)
+for i in range(10, 0, -1):
+    sleep(1)
+    print(i)
+sleep(1)
+print('Boom ****')
