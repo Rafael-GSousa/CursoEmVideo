d72472a7e704e6b6c152503699161ff3915f6003
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-17 19:59:31 -0300

    update desafio 63

--- Python/pythonDesafios/063.py
+++ Python/pythonDesafios/063.py
@@ -1,15 +1,19 @@
 # Escreva um programa que leia um número n inteiro qualquer e mostre na
 # tela os n primeiros elementos de uma Sequência de Fibonacci
-
-n = int(input('Digite um valor: '))
+print('-' * 33)
+print('Sequência de Fibonacci')
+print('-' * 33)
+n = int(input('Quantos termos você quer mostrar? '))
+print('~' * 33)
 primeiro = 0
 segundo = 1
-fibo = 0
+terceiro = 0
 print(f'{primeiro} -> {segundo}', end=' -> ')
-while n - 2 > 0:
-    fibo = primeiro + segundo
-    print(f'{fibo}', end=' -> ')
+while (n - 2) > 0:
+    terceiro = primeiro + segundo
+    print(f'{terceiro}', end=' -> ')
     primeiro = segundo
-    segundo = fibo
+    segundo = terceiro
     n -= 1
 print('FIM')
+print('~' * 33)
