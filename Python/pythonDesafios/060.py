da10136a6edbc10c8399bb887361add202cd7723
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-17 19:08:01 -0300

    update desafio 60

--- Python/pythonDesafios/060.py
+++ Python/pythonDesafios/060.py
@@ -1,12 +1,17 @@
 # Faça um programa que lei um número qualquer e mostre o seu fatorial.
-# Ex.: 5! = 5x4x3x2x1 = 120
+# Ex.: 5! = 5 x 4 x 3 x 2 x 1 = 120
 num = int(input('Digite um número para calcular o seu fatorial: '))
 cont = num
 fat = 1
 if num > 0:
-    while cont >= 1:
-        fat *= cont
-        cont -= 1
-    print(f'O fatorial de {num} é {fat}')
+    print(f'Calculando {num}! = ', end='')
+    # while cont >= 1:
+    for i in range(num, 0, -1):  # Comentar essa linha e ativar o while e vice-versa
+        print(f'{i}', end='')
+        print(f' x ' if i > 1 else ' = ', end='')
+        fat *= i
+        # fat *= cont
+        # cont -= 1
+    print(f'{fat}')
 else:
     print(f'Não existe fatorial de {num}')
