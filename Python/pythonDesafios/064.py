d88ce80cf1926ab2f079d5da4446f52a79915dcc
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-17 02:09:41 -0300

    Add desafio 64

---
+++ Python/pythonDesafios/064.py
@@ -0,0 +1,14 @@
+# Crie um programa que leia vários números inteiros pelo teclado. O
+# programa só vai para quando o usuário digitar o valor 999, que é a condição
+# de parada. No final, mostre quantos números foram digitados e qual foi
+# a soma entre eles (desconsiderando o flag)
+
+n = 0
+cont = 0
+soma = 0
+while n != 999:
+    n = int(input('Digite um valor: '))
+    if n != 999:
+        soma += n
+        cont += 1
+print(f'Você digitou {cont} valores e sua soma foi {soma}')
