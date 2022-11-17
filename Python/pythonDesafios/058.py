e70b7a29c9e0822359adad57b5f73214d324c5d2
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-17 17:27:56 -0300

    update desafio 58

--- Python/pythonDesafios/058.py
+++ Python/pythonDesafios/058.py
@@ -4,17 +4,19 @@
 
 from random import randint
 computador = randint(0, 10)
-jogador = int(input('''Sou seu computador...
+print('''Sou seu computador...
 Acabei de pensar em um número entre 0 e 10.
-Será que você consegue adivinhar qual foi?
-Qual é seu palpite? '''))
-tentativas = 1
-while computador != jogador:
-    if computador > jogador:
-        jogador = int(input('''Mais... Tente mais uma vez.
-Qual é seu palpite? '''))
-    else:
-        jogador = int(input('''Menos... Tente mais uma vez.
-Qual é seu palpite? '''))
+Será que você consegue adivinhar qual foi?''')
+acertou = False
+tentativas = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
     tentativas += 1
+    if computador == jogador:
+        acertou = True
+    else:
+        if computador > jogador:
+            print('Mais... ', end='')
+        else:
+            print('Menos... ', end='')
 print(f'Acertou com {tentativas} tentativas. Parabéns!')
