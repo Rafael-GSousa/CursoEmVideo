3050cdc148c207e38d2ae90d49f8c2a916d809d7
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-17 19:18:37 -0300

    update desafio 61

--- Python/pythonDesafios/061.py
+++ Python/pythonDesafios/061.py
@@ -1,9 +1,12 @@
 # # Refaça o desafio 51, lendo o primeiro termo e a razão de uma PA,
 # mostrando os 10 primeiros termos da progressão usando a estrutura while
-
-pTermo = int(input('1º termo da PA: '))
-razao = int(input('Razão da PA: '))
-uTermo = pTermo + (10 - 1) * razao
-while pTermo <= uTermo:
-    print(pTermo, end=' ')
-    pTermo += razao
+print('Gerador de PA')
+print('-=' * 10)
+pTermo = int(input('1º termo: '))
+razao = int(input('Razão: '))
+termo = pTermo
+cont = 1
+while cont <= 10:
+    print(termo, end=' ')
+    termo += razao
+    cont += 1
