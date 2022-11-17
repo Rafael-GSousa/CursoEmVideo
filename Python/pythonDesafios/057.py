0064c88298f58d803f76befb17708bcdfc778f37
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-17 17:07:27 -0300

    Add desafio 57

--- Python/pythonDesafios/057.py
+++ Python/pythonDesafios/057.py
@@ -2,7 +2,10 @@
 # os valores 'M' ou 'F'. Caso esteja errado, peça a digitação
 # novamente até ter um valor correto.
 
-sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()
-while sexo != 'M' and sexo != 'F':
-    sexo = input('Digite o sexo [M/F]: ').strip().upper()
-print(f'Você digitou o sexo {sexo}')
+sexo = str(input('Digite o seu sexo [M/F]: ')).upper().strip()[0]
+while sexo not in 'MF':
+    sexo = input('Dados inválidos. Por favor, digite o sexo [M/F]: ').strip().upper()[0]
+if sexo == 'M':
+    print(f'Você é do sexo MASCULINO!')
+else:
+    print(f'Você é do sexo FEMININO!')
