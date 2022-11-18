05c3c2d99be6c05124e320f7d9f2f23834ec05e4
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-18 20:03:09 -0300

    update desafio 69

---
+++ Python/pythonDesafios/069.py
@@ -0,0 +1,33 @@
+# Crie um programa que leia a idade e o sexo de várias pessoas. A cada
+# pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não
+# continuar. No final, mostre:
+# A) quantas pessoas tem mais de 18 anos;
+# B) Quantos homens foram cadastrados;
+# C) Quantas mulheres tem menos de 20 anos.
+
+contIdade = contHomens = contMulheres = 0
+while True:
+    print("-" * 40)
+    print(f'{"CADASTRE UMA PESSOA":^40}')
+    print("-" * 40)
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
+    print("-" * 40)
+    while 'M' != sexo != 'F':
+        sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
+    if idade > 18:
+        contIdade += 1
+    if sexo in 'Mm':
+        contHomens += 1
+    elif sexo in 'Ff':
+        if idade < 20:
+            contMulheres += 1
+    continua = str(input('Gostaria de continuar? [S/N]: ')).strip().upper()[0]
+    while 'S' != continua != 'N':
+        continua = str(input('Gostaria de continuar? [S/N]: ')).strip().upper()[0]
+    if continua in 'Nn':
+        break
+print(f'{"FIM DO PROGRAMA":=^40}')
+print(f'{contIdade} pessoas tem mais de 18 anos.')
+print(f'{contHomens} homens foram cadastrados.')
+print(f'{contMulheres} mulheres tem menos de 20 anos.')
