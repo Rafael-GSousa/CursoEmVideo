4d4e50ad123a7d89d6ab28776d5ef60d67ec113e
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-19 13:08:35 -0300

    update desafio 67

--- Python/pythonDesafios/067.py
+++ Python/pythonDesafios/067.py
@@ -1,18 +1,14 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez,
 # para cada valor digitado pelo usuário. O programa será interrompido quando
 # o número solicitado for negativo.
-cont = 0
 while True:
     n = int(input('Digite um número para tabuada (um número negativo ou zero encerra o programa): '))
-    if n <= 0:
+    if n < 0:
         break
-    else:
-        print('-=-' * 10)
-        print(f'Tabuada do {n}')
-        print('-=-' * 10)
-        cont = 1
-        while cont <= 10:
-            print(f'{n} x {cont} = {n * cont}')
-            cont += 1
-        print('---' * 10)
+    print('-=-' * 10)
+    print(f'Tabuada do {n}')
+    print('-=-' * 10)
+    for i in range(1, 11):
+        print(f'{n} x {i} = {n * i}')
+    print('-' * 30)
 print('PROGRAMA DE TABUADA ENCERRADO!')
