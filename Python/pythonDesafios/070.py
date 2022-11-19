3b0563c2ecf1cc3e9b0a418fa5f6459c6c419331
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-19 01:36:41 -0300

    update desafio 70

--- Python/pythonDesafios/070.py
+++ Python/pythonDesafios/070.py
@@ -3,3 +3,31 @@
 # A) Qual é o total gasto na compra;
 # B) Quantos produtos custam mais de R$1000;
 # C) Qual é o nome do produto mais barato.
+print('-' * 30)
+print(f'{"LOJA SUPER BARATÃO":^30}')
+print('-' * 30)
+total = contMil = baratoPreco = contProduto = 0
+baratoNome = ''
+while True:
+    produto = str(input('Nome do Produto: ')).strip().capitalize()
+    preco = float(input('Preço do Produto: R$'))
+    if preco > 1000:
+        contMil += 1
+    total += preco
+    contProduto += 1
+    if contProduto == 1:
+        baratoNome = produto
+        baratoPreco = preco
+    elif baratoPreco > preco:
+        baratoPreco = preco
+        baratoNome = produto
+    continua = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+    if continua in 'N':
+        break
+    else:
+        while continua != 'S':
+            continua = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+print(f'{" FIM DO PROGRAMA ":-^30}')
+print(f'O total da compra foi R${total:.2f}')
+print(f'Temos {contMil} produtos custando mais de R$1000,00')
+print(f'O produto mais barato foi {baratoNome} que custa R${baratoPreco:.2f}')
