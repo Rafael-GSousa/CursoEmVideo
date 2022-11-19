bc90065c8a29c6e30886057d76fb04ddfd5c6bbc
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-19 13:51:00 -0300

    update desafio 69

--- Python/pythonDesafios/069.py
+++ Python/pythonDesafios/069.py
@@ -5,30 +5,29 @@
 # B) Quantos homens foram cadastrados;
 # C) Quantas mulheres tem menos de 20 anos.
 
-contIdade = contHomens = contMulheres = 0
+contIdade = homens = mulheres = 0
 while True:
     print("-" * 40)
     print(f'{"CADASTRE UMA PESSOA":^40}')
     print("-" * 40)
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
-
-    while 'M' != sexo != 'F':
+    sexo = ' '
+    while sexo not in 'FM':
         sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
     if idade > 18:
         contIdade += 1
-    if sexo in 'Mm':
-        contHomens += 1
-    elif sexo in 'Ff':
+    if sexo == 'M':
+        homens += 1
+    if sexo == 'F':
         if idade < 20:
-            contMulheres += 1
+            mulheres += 1
     print("-" * 40)
-    continua = str(input('Gostaria de continuar? [S/N]: ')).strip().upper()[0]
-    while 'S' != continua != 'N':
+    continua = ' '
+    while continua not in 'SN':
         continua = str(input('Gostaria de continuar? [S/N]: ')).strip().upper()[0]
-    if continua in 'Nn':
+    if continua == 'N':
         break
 print(f'{"FIM DO PROGRAMA":=^40}')
 print(f'{contIdade} pessoas tem mais de 18 anos.')
-print(f'{contHomens} homens foram cadastrados.')
-print(f'{contMulheres} mulheres tem menos de 20 anos.')
+print(f'{homens} homens foram cadastrados.')
+print(f'{mulheres} mulheres tem menos de 20 anos.')
