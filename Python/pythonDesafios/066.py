22c1dc4c51e9afb0f8bcc5b1e828ef789ab6cf13
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-19 13:03:47 -0300

    update desafio 66

--- Python/pythonDesafios/066.py
+++ Python/pythonDesafios/066.py
@@ -7,7 +7,6 @@ while True:
     n = int(input('Digite um número (999 para encerrar): '))
     if n == 999:
         break
-    else:
-        cont += 1
-        soma += n
+    cont += 1
+    soma += n
 print(f'Você digitou {cont} números, cuja soma foi {soma}')
