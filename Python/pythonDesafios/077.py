34d524f187f44c8df6db403d26f0330f3476c204
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-21 17:35:24 -0300

    Add desafio 77

--- Python/pythonDesafios/077.py
+++ Python/pythonDesafios/077.py
@@ -1,2 +1,10 @@
 # Crie um programa que tenha uma tupla com várias palavras(não usar acentos).
 # Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+tupla = ('Aprender', 'Programar', 'Linguagem', 'Python', 'Curso', 'Gratis',
+         'Estudar', 'Praticar', 'Trabalhar', 'Mercado', 'Programador', 'Futuro')
+for i in tupla:
+    print(f'Na palavra {i.upper()} temos ', end='')
+    for j in i:
+        if j in "AEIOUaeiou":
+            print(j.lower(), end=' ')
+    print('')
