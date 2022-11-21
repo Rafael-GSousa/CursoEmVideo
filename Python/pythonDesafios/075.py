c2736fc0df156bd114f199f03e30374f0ac52be6
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-21 13:49:46 -0300

    Add desafio 75

--- Python/pythonDesafios/075.py
+++ Python/pythonDesafios/075.py
@@ -4,3 +4,15 @@
 # A) Quantas vezes apareceu o valor 9.
 # B) Em que posição foi digitado o primeiro valor 3.
 # C) Quais foram os números pares.
+
+val1 = int(input('1º Valor: '))
+val2 = int(input('2º Valor: '))
+val3 = int(input('3º Valor: '))
+val4 = int(input('4º Valor: '))
+tupla = (val1, val2, val3, val4)
+print(f'O número 9 apareceu {tupla.count(9)} vezes')
+print(f'O 1º valor 3 está {tupla.index(3) + 1}ª posição')
+print(f'Os números pares foram: ', end='')
+for i in tupla:
+    if i % 2 == 0:
+        print(i, end=' ')
