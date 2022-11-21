5a391847819eba79100e5efeab24d00116b51231
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-21 18:09:01 -0300

    update desafio 73

--- Python/pythonDesafios/073.py
+++ Python/pythonDesafios/073.py
@@ -4,18 +4,17 @@
 # b) Os últimos 4 colocados.
 # c) Times em ordem alfabética.
 # d) Em que posição está o time da Chapecoense.
-tabela1 = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro')
-tabela2 = ('Flamengo', 'Vasco', 'Chapecoense', 'Atlético - MG', 'Botafogo')
-tabela3 = ('Atlético - PR', 'Bahia', 'São Paulo', 'Fluminense', 'Sport')
-tabela4 = ('Vitória', 'Coritiba', 'Avaí', 'Ponte Preta', 'Atlético - GO')
-tabela5 = tabela1 + tabela2 + tabela3 + tabela4
+tabela = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro',
+          'Flamengo', 'Vasco', 'Chapecoense', 'Atlético - MG', 'Botafogo',
+          'Atlético - PR', 'Bahia', 'São Paulo', 'Fluminense', 'Sport',
+          'Vitória', 'Coritiba', 'Avaí', 'Ponte Preta', 'Atlético - GO')
 print('-=' * 20)
-print(f'Lista de times do Brasileirão 2018: {tabela5}')
+print(f'Lista de times do Brasileirão 2018: {tabela}')
 print('-=' * 20)
-print(f'Os 5 primeiros são: {tabela5[:5]}')
+print(f'Os 5 primeiros são: {tabela[:5]}')
 print('-=' * 20)
-print(f'Os 4 útlimos são: {tabela5[16:]}')
+print(f'Os 4 útlimos são: {tabela[16:]}')
 print('-=' * 20)
-print(f'Times em ordem alfabética: {sorted(tabela5)}')
+print(f'Times em ordem alfabética: {sorted(tabela)}')
 print('-=' * 20)
-print(f'A Chapecoense está na {tabela5.index("Chapecoense") + 1}ª posição')
+print(f'A Chapecoense está na {tabela.index("Chapecoense") + 1}ª posição')
