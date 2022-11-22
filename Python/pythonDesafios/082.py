639ba48b0f7457954d98212c66a27c62a09410a1
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-22 18:20:37 -0300

    Add desafio 82

---
+++ Python/pythonDesafios/082.py
@@ -0,0 +1,18 @@
+lista = list()
+l2 = list()
+l3 = list()
+while True:
+    lista.append(int(input('Digite um valor: ')))
+    continua = ' '
+    while continua not in 'SN':
+        continua = str(input('Quer continuar? [S/N]: ')).upper().strip()
+    if continua in 'N':
+        break
+for i in range(0, len(lista)):
+    if lista[i] % 2 == 0:
+        l2.append(lista[i])
+    else:
+        l3.append(lista[i])
+print(f'Lista geral: {lista}')
+print(f'Lista dos pares: {l2}')
+print(f'Lista dos ímpares: {l3}')
