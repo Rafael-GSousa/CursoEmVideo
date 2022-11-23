fe30772b228e4ad8cc49202dd3ed128e3f5ea0ac
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-23 00:13:34 -0300

    update desafio 81

--- Python/pythonDesafios/081.py
+++ Python/pythonDesafios/081.py
@@ -1,16 +1,13 @@
 lista = list()
-cont = 0
 while True:
     lista.append(int(input('Digite um valor: ')))
-    cont += 1
     continua = ' '
     while continua not in 'SN':
         continua = str(input('Quer continuar? [S/N]: ')).upper().strip()
     if continua in 'N':
         break
 print(f'Você digitou os valores: {lista}')
-print(f'Foram digitados {cont} valores')
-lista.sort()
+print(f'Foram digitados {len(lista)} valores')
 lista.sort(reverse=True)
 print(f'Lista ordenada de forma decrescente: {lista}')
 if 5 in lista:
