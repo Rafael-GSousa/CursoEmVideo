5045ca408b45195be12ee071e00b5bc09eb3e5d8
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-27 00:03:42 -0300

    update desafio 86

--- Python/pythonDesafios/086.py
+++ Python/pythonDesafios/086.py
@@ -6,7 +6,8 @@ for i in range(0, 3):
         aux.append(n)
     matriz.append(aux[:])
     aux.clear()
-print(matriz)
 for i in matriz:
     for j in i:
-        print(f'{j}', end=' ')
+        print(f'[{j}]', end=' ')
+    if i.index(j) == 2:
+        print()
