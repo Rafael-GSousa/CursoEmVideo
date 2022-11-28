bba8bfd088bec20085cfa7bb9c56cfa10c6aeba7
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-28 03:31:40 -0300

    update desafio 86

--- Python/pythonDesafios/086.py
+++ Python/pythonDesafios/086.py
@@ -1,8 +1,7 @@
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-for i in range(0, 3):
-    for j in range(0, 3):
-        n = int(input(f'Digite um valor para a posição [{i}, {j}]: '))
-        matriz[i] = n
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input(f'Digite um valor para a posição [{linha}, {coluna}]: '))
 for i in matriz:
     for j in i:
         print(f'[ {j} ]', end=' ')
