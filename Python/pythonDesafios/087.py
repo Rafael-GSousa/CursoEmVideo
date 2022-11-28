682461b9bc870473575d3536d47433b6c45c116a
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-28 03:57:04 -0300

    update desafio 87

--- Python/pythonDesafios/087.py
+++ Python/pythonDesafios/087.py
@@ -1,20 +1,18 @@
-matriz = [[], [], []]
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 somaPares = somaTerceiraColuna = maior = 0
 for i in range(0, 3):
     for j in range(0, 3):
-        n = int(input(f'Digite um valor para a posição [{i}, {j}]: '))
-        if n % 2 == 0:
-            somaPares += n
+        matriz[i][j] = int(input(f'Digite um valor para a posição [{i}, {j}]: '))
+        if matriz[i][j] % 2 == 0:
+            somaPares += matriz[i][j]
         if j == 2:
-            somaTerceiraColuna += n
+            somaTerceiraColuna += matriz[i][j]
         if i == 1:
             if j == 0:
-                maior = n
-            elif n > maior:
-                maior = n
-        matriz[i].append(n)
+                maior = matriz[i][j]
+            elif matriz[i][j] > maior:
+                maior = matriz[i][j]
 print('-=' * 20)
-print(f'{"Matriz":^20}')
 for i in matriz:
     for j in i:
         print(f'[{j:^10}]', end=' ')
