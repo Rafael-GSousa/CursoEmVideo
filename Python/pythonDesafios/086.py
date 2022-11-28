75c268207c122de3f271bca02f9bd0d03b238b0d
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-28 03:29:20 -0300

    update desafio 86

--- Python/pythonDesafios/086.py
+++ Python/pythonDesafios/086.py
@@ -1,8 +1,8 @@
-matriz = [[], [], []]
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for i in range(0, 3):
     for j in range(0, 3):
         n = int(input(f'Digite um valor para a posição [{i}, {j}]: '))
-        matriz[i].append(n)
+        matriz[i] = n
 for i in matriz:
     for j in i:
         print(f'[ {j} ]', end=' ')
