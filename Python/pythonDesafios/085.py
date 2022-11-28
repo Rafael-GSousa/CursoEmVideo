e2b175ffbb6c871b9432a3abe7ba2fc3355a84d8
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-11-28 03:11:51 -0300

    update desafio 85

--- Python/pythonDesafios/085.py
+++ Python/pythonDesafios/085.py
@@ -1,15 +1,12 @@
-li = list()
-lp = list()
-valores = [li, lp]
+valores = [[], []]
 for i in range(1, 8):
     n = int(input(f'Digite o {i}º valor: '))
     if n % 2 == 0:
-        lp.append(n)
-        lp.sort()
+        valores[0].append(n)
     else:
-        li.append(n)
-        li.sort()
+        valores[1].append(n)
 print('-=' * 30)
-print(f'Lista completa : {valores}')
-print(f'Os valores pares são: {lp}')
-print(f'Os valores impares são: {li}')
+valores[0].sort()
+valores[1].sort()
+print(f'Os valores pares são: {valores[0]}')
+print(f'Os valores impares são: {valores[1]}')
